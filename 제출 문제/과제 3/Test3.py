4ab4fec408197acc83a1ca72a270dfe5d07b6f90
Jo <chjo70@hotmail.com>
2018-09-24 12:21:08 +0900

    test

--- 제출 문제/과제 3/Test3.py
+++ 제출 문제/과제 3/Test3.py
@@ -2,16 +2,43 @@
 
 def CountOfChar( line ):
 
+    co = 0;
+    for ch in line:
+        #print( ch )
+        if ch != ' ':
+            co = co + 1
+    return co;
 
-    return 0;
+def CountOfWord( line ):
+
+    co = 0;
+    s = line.replace("\n", "")
+    s = s.split(" ")
+    #print( s )
+    co = len( s )
+    nullspace = s.count('')
+    #print( co, nullspace )
+    return co - nullspace;
 
 
 ##############################################
-f = open( "fire.txt", 'r' )
-lines = f.readlines()
-for line in lines:
-    # print(line)
-    coc = CountOfChar(line)
-f.close()
-print( '문자 수 :' , coc )
+filenames = [ 'fire.txt', 'wheel.txt', 'lever.txt', 'agriculture.txt', 'earthware.txt'  ]
 
+for filename in filenames:
+    coc = 0;
+    cow = 0;
+    meanw = 0.0;
+    print( filename )
+    f = open( filename, 'r' )
+    lines = f.readlines()
+    for line in lines:
+        # print(line)
+        coc = CountOfChar(line) + coc;
+        cow = CountOfWord(line) + cow;
+    f.close()
+
+    meanw = coc / cow;
+
+    print( '- total characters: ' , coc )
+    print( '- total words: ', cow )
+    print( '- average characters per words: ', '%.3f' % meanw )
